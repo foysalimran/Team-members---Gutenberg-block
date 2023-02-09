182a48dfbcc1569567691aa45c0294ed3737a69f
Foysal Imran <fimran2011@gmail.com>
2023-02-09 15:43:29 +0600

    added styleing frontend

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd496f218442b18733bcb');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'af317b93ce8c465a5b77');
