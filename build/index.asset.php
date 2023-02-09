b318eec1c57e795c797c19834396f7a4d04b2a20
Foysal Imran <fimran2011@gmail.com>
2023-02-09 15:05:47 +0600

    styleing icons

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c58dd4f4b8a074996256');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e09bc636046a07d0df4a');
