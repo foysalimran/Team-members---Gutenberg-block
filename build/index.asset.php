b8acf4411d08a6b2b8dcaa990ef670137d4e8337
Foysal Imran <fimran2011@gmail.com>
2023-02-17 20:14:41 +0600

    update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6440eb9f6eafd8e7d980');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e461dc028e4fd14bfab3');
